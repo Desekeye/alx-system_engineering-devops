37976226e8cfb8bdf39662e7bbb9f5de96f916c2
Desekeye <106808436+Desekeye@users.noreply.github.com>
2023-01-11 20:28:08 +0100

    puppet

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,6 @@
-# installs flask from pip3 Puppet
+# installs flask from pip3 using Puppet
 
-exec {'install flask':
-    command => /usr/bin/pip3 install flask == 2.1.0,
+package {'flask':
+  ensure   => '2.1.0',
+  provider => 'pip3',
 }
