047bacb022bee9206774674ad936375d781db6b7
Desekeye <106808436+Desekeye@users.noreply.github.com>
2023-01-11 20:15:40 +0100

    puppet

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,10 @@
-# creates a file in /tmp using Puppet
-file {'/tmp/school':
-  ensure => file,
-  mode => '0744',
-  owner => 'www.data',
-  group => 'www.data',
-  content => 'I love puppet'
+# Create a file in /tmp using Puppet
+
+file { 'school':
+  ensure  => 'present',
+  content => 'I love Puppet',
+  group   => 'www-data',
+  mode    => '0744',
+  owner   => 'www-data',
+  path    => '/tmp/school',
 }
