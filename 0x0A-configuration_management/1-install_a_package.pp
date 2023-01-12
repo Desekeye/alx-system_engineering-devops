976ca5382552d3d2aa715d93c196785775f98e03
Desekeye <triciadede024@gmail.com>
2023-01-12 16:04:42 +0000

    wsd0

--- 0x0A-configuration_management/1-install_a_package.pp
+++
@@ -1,6 +0,0 @@
-# installs flask from pip3 using Puppet
-
-package {'flask':
-  ensure   => '2.1.0',
-  provider => 'pip3',
-}
--- 0x0A-configuration_management/2-execute_a_command.pp
+++
@@ -1,4 +0,0 @@
-# creates a manifest that kills a process killmenow using Puppet
-exec {'pkill killmenow':
-  path  => '/usr/bin:/usr/sbin:/bin',
-}
