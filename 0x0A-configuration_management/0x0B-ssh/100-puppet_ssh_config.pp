404b15a9999fc5866ef9ec5c8e5cc507a71f4cc4
Desekeye <triciadede024@gmail.com>
2023-01-12 13:13:25 +0000

    ssh

--- 0x0A-configuration_management/0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0A-configuration_management/0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,1 +1,12 @@
-#!/usr/bin/env bash
+# configuration file (w/ Puppet)
+file_line {'password-authenticate':
+  ensure => 'present',
+  path    => '/etc/ssh/ssh_config',
+  line   => '    PasswordAuthentication no'
+}
+
+file_line {'private-key':
+  ensure => 'present',
+  path    => '/etc/ssh/ssh_config',
+  line    => '    IdentityFile ~/.ssh/school',
+}
